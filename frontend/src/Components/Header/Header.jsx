3bb24b908670fcb2f9527436c31683728f20bcd4
Abdul <iraheemishfaq@gmail.com>
2025-08-25 21:25:28 +0500

    google oauth

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className="header">
         <div className="header-contents">
-            
+            <h2>Order Your Favourites Food Here</h2>
             <p>Choose from a diverse menu featuring delectable array of dishes crafted with the finest 
              ingredients an culinary expertise . Our Mission is to satisfy your cravings and elevate your dininr experiences ,
               one delicious meal at a time. 
